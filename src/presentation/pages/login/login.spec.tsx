668aafc37b9ac8cbd40e7fba6907de441aa7c41d
Leonardo <leoleles9159@hotmail.com>
2022-07-06 19:22:06 -0300

    test: ensure Login present valid state if email is valid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -60,6 +60,17 @@ describe('Login Component', () => {
     expect(passwordStatus.title).toBe(validationStub.errorMessage)
   })
 
+  test('Should show valid email state if Validation succeds', () => {
+    const { sut, validationStub } = makeSut()
+    validationStub.errorMessage = null
+    const emailInput = sut.getByTestId('email')
+    fireEvent.input(emailInput, { target: { value: faker.internet.email() } })
+
+    const emailStatus = sut.getByTestId('email-status')
+
+    expect(emailStatus.title).toBe('Tudo certo!')
+  })
+
   test('Should show valid password state if Validation succeds', () => {
     const { sut, validationStub } = makeSut()
     validationStub.errorMessage = null
