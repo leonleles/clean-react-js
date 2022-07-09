387e5a4d47b8e8dc07baf010cbed358874907d64
Leonardo Leles <leoleles91593@gmail.com>
2022-07-09 18:48:48 -0300

    refactor: rename MinLengthValidator

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -1,9 +1,9 @@
 import { InvalidFieldError } from '@/validation/errors'
-import { MINLengthValidation } from './min-length-validation'
+import { MinLengthValidation } from './min-length-validation'
 
-describe('MINLengthValidation', () => {
+describe('MinLengthValidation', () => {
   test('Should return error if value is invalid', () => {
-    const sut = new MINLengthValidation('field', 5)
+    const sut = new MinLengthValidation('field', 5)
     const isValid = sut.validate('123')
 
     expect(isValid).toEqual(new InvalidFieldError())
--- src/validation/validators/min-length/min-length-validation.ts
+++ src/validation/validators/min-length/min-length-validation.ts
@@ -1,7 +1,7 @@
 import { InvalidFieldError } from '@/validation/errors'
 import { FieldValidation } from '@/validation/protocols/field-validation'
 
-export class MINLengthValidation implements FieldValidation {
+export class MinLengthValidation implements FieldValidation {
   constructor (readonly field: string, private readonly minLength: number) {}
 
   validate (value: string): Error {
