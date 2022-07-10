36f43cc0b70a83b78998a0469d7b0da4ad0ed5dd
Leonardo Leles <leoleles91593@gmail.com>
2022-07-10 17:57:05 -0300

    refactor: move SUT creatin to a factory method

--- src/infra/cache/cache/local-storage-adapter.spec.ts
+++ src/infra/cache/cache/local-storage-adapter.spec.ts
@@ -3,6 +3,8 @@ import faker from 'faker'
 import 'jest-localstorage-mock'
 import { LocalStorageAdapter } from './local-storage-adapter'
 
+const makeSut = (): LocalStorageAdapter => new LocalStorageAdapter()
+
 describe('LocalStorageAdapter', () => {
   afterEach(cleanup)
   beforeEach(() => {
@@ -10,7 +12,7 @@ describe('LocalStorageAdapter', () => {
   })
 
   test('Should call localStorage with correct values', async () => {
-    const sut = new LocalStorageAdapter()
+    const sut = makeSut()
     const key = faker.database.column()
     const value = faker.random.word()
 
