28dbc72c143a00bd60c58a8dfeb4ded3bd333d32
Leonardo Leles <leoleles91593@gmail.com>
2022-07-19 19:48:08 -0300

    feat: ensure SurveyList LoadAll and Initial state

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import { LoadSurveyList } from '@/domain/usecases'
+import { Footer, Header } from '@/presentation/components'
+import { SurveyItemEmpty } from '@/presentation/pages/survey-list/components'
+import React, { useEffect } from 'react'
 import Styles from './survey-list-styles.scss'
-import { Header, Footer } from '@/presentation/components'
 
-const SurveyList: React.FC = () => {
+type Props = {
+  loadSurveyList: LoadSurveyList
+}
+
+const SurveyList: React.FC<Props> = ({ loadSurveyList }) => {
+  useEffect(() => {
+    (async function () {
+      loadSurveyList.loadAll()
+    })()
+  }, [])
+
   return (
     <div className={Styles.surveyListWrap}>
       <Header />
       <div className={Styles.contentWrap}>
         <h2>Enquetes</h2>
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
+        <ul data-testid="survey-list">
+          <SurveyItemEmpty/>
         </ul>
       </div>
       <Footer />
