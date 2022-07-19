6e0e7a70913fc7b0b01666afac860bcb97f8fd0f
Leonardo Leles <leoleles91593@gmail.com>
2022-07-19 19:48:31 -0300

    test: ensure SurveyList LoadAll and Initial state

---
+++ src/presentation/pages/survey-list/components/survey-list.spec.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import { SurveyList } from '@/presentation/pages'
+import { LoadSurveyList } from '@/domain/usecases'
+import { SurveyModel } from '@/domain/models'
+
+class LoadSurveyListSpy implements LoadSurveyList {
+  callCount = 0
+  async loadAll (): Promise<SurveyModel[]> {
+    this.callCount++
+    return []
+  }
+}
+
+type SutTypes = {
+  loadSurveyListSpy: LoadSurveyListSpy
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyListSpy = new LoadSurveyListSpy()
+  render(<SurveyList loadSurveyList={loadSurveyListSpy}/>)
+
+  return {
+    loadSurveyListSpy
+  }
+}
+
+describe('SurveyList Component', () => {
+  test('Should present 4 empty items on start', () => {
+    makeSut()
+    const surveyList = screen.getByTestId('survey-list')
+    expect(surveyList.querySelectorAll('li:empty').length).toBe(4)
+  })
+  test('Should call LoadSurveyList', () => {
+    const { loadSurveyListSpy } = makeSut()
+    expect(loadSurveyListSpy.callCount).toBe(1)
+  })
+})
