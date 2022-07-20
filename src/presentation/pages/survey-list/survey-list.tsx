22482e194900902906dab1132ff26d64c5baf174
Leonardo Leles <leoleles91593@gmail.com>
2022-07-19 21:10:04 -0300

    feat: ensure SurveyList render error on failure

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,7 +1,10 @@
 import { SurveyModel } from '@/domain/models'
 import { LoadSurveyList } from '@/domain/usecases'
 import { Footer, Header } from '@/presentation/components'
-import { SurveyItemEmpty, SurveyItem } from '@/presentation/pages/survey-list/components'
+import {
+  SurveyItemEmpty,
+  SurveyItem
+} from '@/presentation/pages/survey-list/components'
 import React, { useEffect, useState } from 'react'
 import Styles from './survey-list-styles.scss'
 
@@ -10,10 +13,16 @@ type Props = {
 }
 
 const SurveyList: React.FC<Props> = ({ loadSurveyList }) => {
-  const [state, setState] = useState({ surveys: [] as SurveyModel[] })
+  const [state, setState] = useState({
+    surveys: [] as SurveyModel[],
+    error: ''
+  })
 
   useEffect(() => {
-    loadSurveyList.loadAll().then(surveys => setState({ surveys }))
+    loadSurveyList
+      .loadAll()
+      .then((surveys) => setState({ ...state, surveys }))
+      .catch((error) => setState({ ...state, error: error.message }))
   }, [])
 
   return (
@@ -21,12 +30,26 @@ const SurveyList: React.FC<Props> = ({ loadSurveyList }) => {
       <Header />
       <div className={Styles.contentWrap}>
         <h2>Enquetes</h2>
-        <ul data-testid="survey-list">
-          {state.surveys.length
-            ? state.surveys.map((survey: SurveyModel) => <SurveyItem key={survey.id} survey={survey} />)
-            : <SurveyItemEmpty/>}
-
-        </ul>
+        {state.error
+          ? (
+          <div>
+            <span data-testid="error">{state.error}</span>
+            <button>Recarregar</button>
+          </div>
+            )
+          : (
+          <ul data-testid="survey-list">
+            {state.surveys.length
+              ? (
+                  state.surveys.map((survey: SurveyModel) => (
+                <SurveyItem key={survey.id} survey={survey} />
+                  ))
+                )
+              : (
+              <SurveyItemEmpty />
+                )}
+          </ul>
+            )}
       </div>
       <Footer />
     </div>
